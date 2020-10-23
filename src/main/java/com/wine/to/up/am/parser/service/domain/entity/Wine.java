e8c8a7e4ad3f991b3a2586cea9c5f08f01c613ab
Semyon Kulikov <senich.99@gmail.com>
2020-10-23 09:04:34 +0300

    Fixed init migration

--- src/main/java/com/wine/to/up/am/parser/service/domain/entity/Wine.java
+++ src/main/java/com/wine/to/up/am/parser/service/domain/entity/Wine.java
@@ -11,6 +11,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
@@ -27,7 +28,7 @@ import java.util.List;
 public class Wine {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     @ApiModelProperty(notes = "The database generated wine ID")
     private long id;
