fa4ff6976d62f90c0e3e8d8c027423addd69d51f
SSyrova <isaisa553@gmail.com>
2020-11-02 20:28:52 +0300

    Added endpoint to return wines as json.

--- src/main/java/com/wine/to/up/am/parser/service/service/impl/RestServiceImpl.java
+++ src/main/java/com/wine/to/up/am/parser/service/service/impl/RestServiceImpl.java
@@ -1,12 +1,17 @@
 package com.wine.to.up.am.parser.service.service.impl;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.wine.to.up.am.parser.service.model.dto.WineDto;
 import com.wine.to.up.am.parser.service.service.RestService;
 import com.wine.to.up.am.parser.service.service.SearchService;
 import com.wine.to.up.am.parser.service.service.UpdateService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
 
 /**
@@ -41,5 +46,14 @@ public class RestServiceImpl implements RestService {
     public void updateAll() {
         updateService.updateDictionary();
         updateService.updateWines();
+    }
+
+    @Override
+    public void readAsFile(HttpServletResponse httpServletResponse) throws IOException {
+        List<WineDto> wines = searchService.findAll();
+        httpServletResponse.setContentType("application/json");
+        PrintWriter out = httpServletResponse.getWriter();
+        out.print(new ObjectMapper().writeValueAsString(wines));
+        out.flush();
     }
 }
