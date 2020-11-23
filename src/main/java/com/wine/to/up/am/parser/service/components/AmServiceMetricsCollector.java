2e2c9dcd14b5b254d40b4d63bbe66f2000dd9d45
vovalexeev <56478373+vovalexeev@users.noreply.github.com>
2020-11-23 14:54:29 +0300

    Added namespace parameter in building metrics

--- src/main/java/com/wine/to/up/am/parser/service/components/AmServiceMetricsCollector.java
+++ src/main/java/com/wine/to/up/am/parser/service/components/AmServiceMetricsCollector.java
@@ -31,34 +31,42 @@ public class AmServiceMetricsCollector extends CommonMetricsCollector {
     private static final String JOB_EXECUTION_TIME = "job_execution_time";
 
     private static final Gauge parsedWinesSuccessGauge = Gauge.build()
+            .namespace(SERVICE_NAME)
             .name(PARSED_WINES_SUCCESS)
             .help("Number of successfully parsed wines")
             .register();
     private static final Gauge winesParsedUnsuccessfulGauge = Gauge.build()
+            .namespace(SERVICE_NAME)
             .name(WINES_PARSED_UNSUCCESSFUL)
             .help("Number of unsuccessfully parsed wines")
             .register();
     private static final Gauge isBannedGauge = Gauge.build()
+            .namespace(SERVICE_NAME)
             .name(IS_BANNED)
             .help("Is banned host's IP")
             .register();
     private static final Counter numberOfWinesCreatedCounter = Counter.build()
+            .namespace(SERVICE_NAME)
             .name(NUMBER_OF_WINES_CREATED)
             .help("Number of created wines during 1 parsing")
             .register();
     private static final Counter numberOfWinesUpdatedCounter = Counter.build()
+            .namespace(SERVICE_NAME)
             .name(NUMBER_OF_WINES_UPDATED)
             .help("Number of updated wines during 1 parsing")
             .register();
     private static final Counter numberOfWinesDeletedCounter = Counter.build()
+            .namespace(SERVICE_NAME)
             .name(NUMBER_OF_WINES_DELETED)
             .help("Number of deleted wines during 1 parsing (due to irrelevance)")
             .register();
     private static final Gauge percentageOfUnsuccessfullyParsedWinesGauge = Gauge.build()
+            .namespace(SERVICE_NAME)
             .name(PERCENTAGE_OF_UNSUCCESSFULLY_PARSED_WINES)
             .help("Percent of unsuccessfully parsed wines")
             .register();
     private static final Summary jobExecutionTimeSummary = Summary.build()
+            .namespace(SERVICE_NAME)
             .name(JOB_EXECUTION_TIME)
             .help("Job execution time")
             .register();
