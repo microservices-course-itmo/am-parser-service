1ceb57e751feb73eaee42d07e125fd6770ba71e5
SSyrova <isaisa553@gmail.com>
2020-11-30 14:05:16 +0300

    Added proxy

--- src/main/java/com/wine/to/up/am/parser/service/configuration/ClientConfiguration.java
+++ src/main/java/com/wine/to/up/am/parser/service/configuration/ClientConfiguration.java
@@ -3,8 +3,13 @@ package com.wine.to.up.am.parser.service.configuration;
 import com.wine.to.up.am.parser.service.components.AmServiceMetricsCollector;
 import com.wine.to.up.am.parser.service.service.AmClient;
 import com.wine.to.up.am.parser.service.service.AmService;
+import com.wine.to.up.am.parser.service.service.ProxyClient;
+import com.wine.to.up.am.parser.service.service.ProxyService;
 import com.wine.to.up.am.parser.service.service.impl.AmClientImpl;
 import com.wine.to.up.am.parser.service.service.impl.AmServiceImpl;
+import com.wine.to.up.am.parser.service.service.impl.ProxyClientImpl;
+import com.wine.to.up.am.parser.service.service.impl.ProxyServiceImpl;
+import org.checkerframework.checker.units.qual.A;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,8 +17,18 @@ import org.springframework.context.annotation.Configuration;
 public class ClientConfiguration {
 
     @Bean
-    public AmClient amClient() {
-        return new AmClientImpl();
+    public ProxyClient proxyClient() {
+        return new ProxyClientImpl();
+    }
+
+    @Bean
+    public ProxyService proxyService(ProxyClient proxyClient) {
+        return new ProxyServiceImpl(proxyClient);
+    }
+
+    @Bean
+    public AmClient amClient(ProxyService proxyService, AmServiceMetricsCollector amServiceMetricsCollector) {
+        return new AmClientImpl(proxyService, amServiceMetricsCollector);
     }
 
     @Bean
