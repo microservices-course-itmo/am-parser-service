02c775355b261990b32053db7c1e9a684b35b9ab
bgubanov <b.gubanov@list.ru>
2021-03-21 14:38:57 +0300

    Fix import for Eureka

--- src/main/java/com/wine/to/up/am/parser/service/ServiceApplication.java
+++ src/main/java/com/wine/to/up/am/parser/service/ServiceApplication.java
@@ -2,6 +2,7 @@ package com.wine.to.up.am.parser.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
